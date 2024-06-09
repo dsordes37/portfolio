e7dc2ef1fe0fc224b5ada5a6957fcd6edc8e4f6f
D'Sordes <125225427+dsordes37@users.noreply.github.com>
2024-06-09 10:53:48 -0300

    version 1.2

--- src/app/pages/project/project.component.ts
+++ src/app/pages/project/project.component.ts
@@ -16,8 +16,9 @@ export class ProjectComponent implements OnInit {
   text:string='Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quas quibusdam quae veritatis a nobis esse aperiam, ipsum, reprehenderit praesentium nulla facilis corrupti quam iusto, quasi possimus enim doloremque deserunt. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur quod sed placeat voluptatibus. Illo, corporis, suscipit mollitia nihil fuga porro reiciendis, enim voluptatibus saepe ipsa molestiae harum magni modi aliquid? Lorem ipsum dolor sit amet consectetur adipisicing elit. Id totam magnam harum ex accusantium beatae maiores obcaecati repellendus architecto tempora atque cumque vitae dignissimos dolorem odit nulla, iste quidem esse? Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi ipsam nobis perspiciatis aspernatur! Blanditiis maiores dolorem ab aspernatur recusandae autem. Magni sed vitae ea quis, incidunt expedita saepe est impedit.';
   ano:string='';
   demo:string='';
-  github:string=''
-  inspirations:{url:string, name:string}[]|null=null
+  github:string='';
+  inspirations:{url:string, name:string}[]|null=null;
+  logo:string='';
 
   constructor(private route:ActivatedRoute) {
     this.route.paramMap.subscribe(value=>this.id=value.get('id'));
@@ -38,6 +39,8 @@ export class ProjectComponent implements OnInit {
     this.demo=result.demo_url;
     this.github=result.github_url;
     this.inspirations=result.inspirations;
+    this.text=result.text;
+    this.logo=result.logo;
 
   }
 
--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -6,11 +6,16 @@ export const projects:any[]=[
         year:'2024',
         laptop_img:"../assets/images/portfolio_laptop.png",
         cell_img:"../assets/images/portfolio_cell.png",
-        demo_url:"https://dsordes.netlify.app",
+        demo_url:"",
         github_url:"https://github.com/dsordes37/portfolio",
-        inspirations:null
+        inspirations:null,
+        text:"<p>A criação de um portfolio é um evento canônico na vida de todo desenvolvedor de software, comigo não seria diferente. Essa site foi feito com angular a partir de um design de minha autoria e busca mostrar ao usuário e possível contratante informações sobre mim e sobre minhas habilidades. A aplicação usa typescript para interpretar listas de informações, contendo nomes, imagens e links dos meus projetos, e organiza-las na página e utiliza angular routes para possibilitar a navegação rápida entre a página principal e as páginas de projetos.</p>",
+        logo:"../assets/images/logo.png"
+
+
         
     },
+
     {
         id:'1',
         name:"the blog",
@@ -21,17 +26,18 @@ export const projects:any[]=[
         github_url:"https://github.com/dsordes37/blog",
         inspirations:[
             {
-                url:'url',
-                name:'url'
+                url:'https://dribbble.com/dwinawan',
+                name:'Dwinawan dribbble'
             },{
-                url:'url',
-                name:'url'
-            },{
-                url:'url',
-                name:'url'
-            },
-        ]
+                url:'https://dribbble.com/shots/18089191-Blog-Layout',
+                name:'Design'
+            }
+        ],
+        text:"<p>Esse projeto foi feito a partir do design de Dwinawan Hariwijaya como atividade prática em um bootcamp sobre angular da plataforma DIO. O site usa typescript para ler os dados listados em um arquivo local e os organiza em forma de cards, que são distribuidos na página principal e levam, por meio do angular routes, a páginas secundárias, que seriam as páginas dos artigos.</p> <p>Um caso de uso possível para essa aplicação seria como landing page de um noticiário online, bastando apenas substituir as informações hospedadas localmente pelo uso de requisições a uma API de notícias.</p>",
+        logo:"../assets/images/blog_logo.png"
+
     },
+
     {
         id:'2',
         name:"translater",
@@ -42,9 +48,20 @@ export const projects:any[]=[
         github_url:"https://github.com/dsordes37/translate_app",
         inspirations:[
             {
-                url:'',
-                name:''
+                url:'https://devchallenges.io/editor/solution/14923',
+                name:'Desafio'
+            },{
+                url:'https://devchallenges.io/',
+                name:'Devchallenges'
+            },{
+                url:'https://mymemory.translated.net/doc/spec.php',
+                name:'MyMemory API'
             }
-        ]
+        ],
+        text:"<p>Esse site foi feito a partir de um desafio frontend do site devchallenges.io como forma de ganhar experiência em angular. A aplicação utiliza typescript para integrar-se como a API de tradução de texto do MyMemory, além de outras funcionalidades como, contar número de caracteres, trocar o idioma a ser traduzido e o idioma da tradução e copiar o texto.</p>",
+        logo:"../assets/images/translate_logo.png"
+
+
     }
+
 ]
